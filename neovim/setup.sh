af9dac6b6786ce4c089912bd5af0b1f8dcb654ff
Matthew Gray <matthewphilipgray+github@gmail.com>
2021-11-23 19:43:11 +0000

    Set neovim as the default git commit editor

--- neovim/setup.sh
+++ neovim/setup.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# Setup the config 
 mkdir -p ~/.config/nvim
 cp ./neovim/init.vim ~/.config/nvim/init.vim
+
+# Set neovim as the default git interpreter
+git config --global core.editor "nvim"
 
