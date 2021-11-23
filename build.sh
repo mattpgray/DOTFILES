eba62f67cf3ec6f036009bbd5c9cd59c57b2ea82
Matthew Gray <matthewphilipgray+github@gmail.com>
2021-11-23 19:40:34 +0000

    Setup neovim

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -ex
 
-# install package with apt
-# sudo apt update
-# sudo apt install -y git-all
-# sudo apt install -y neovim
+# Get the path to this file. Needed later when we use the other files relative to this one
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+install package with apt
+sudo apt update
+sudo apt install -y git-all
+sudo apt install -y neovim
 # Git credentials manager
 sudo apt install -y libsecret-1-0 libsecret-1-dev
 cd /usr/share/doc/git/contrib/credential/libsecret
 sudo make
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
+# Setup other packages
+cd $SCRIPTPATH
+. ./neovim/setup.sh
+
---
+++ neovim/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p ~/.config/nvim
+cp ./neovim/init.vim ~/.config/nvim/init.vim
+
