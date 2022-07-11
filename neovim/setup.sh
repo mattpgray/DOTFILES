bb24bacac33bf642dfb26ed5b58fd01d57502fb1
Matthew Gray <mgray@europe.altair.com>
2022-07-11 10:35:32 +0100

    Fix calling neovim setup from any directory

--- neovim/setup.sh
+++ neovim/setup.sh
@@ -3,21 +3,17 @@
 set -ex
 
 # Get the path to this file. Needed later when we use the other files relative to this one
-OLD_WD=${PWD}
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd ${SCRIPTPATH}
+SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+echo $SCRIPT_PATH
 
 # Setup the config 
 mkdir -p ~/.config/nvim
-cp ./init.vim ~/.config/nvim/init.vim
+cp ${SCRIPT_PATH}/init.vim ~/.config/nvim/init.vim
 
 # Set neovim as the default git interpreter
 git config --global core.editor "nvim"
 
 # Move the plugins to the correct dir
 mkdir -p ~/.config/nvim/plugins
-cp ./plugins/* ~/.config/nvim/plugins
-
-# Fix state
-cd ${OLD_WD}
+cp ${SCRIPT_PATH}/plugins/* ~/.config/nvim/plugins
 
