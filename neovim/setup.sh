18f4755e01506222482e6a492a6308a1b4ec6ebf
= <=>
2023-01-07 11:06:32 +0000

    Packer was not loaded and the remaps were not in the correct order

--- neovim/setup.sh
+++ neovim/setup.sh
@@ -5,5 +5,5 @@ set -ex
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 rm -fr ~/.config/nvim
-ln -s ${SCRIPT_DIR}/config ~/.config/nvim
+cp -r ${SCRIPT_DIR}/config ~/.config/nvim
 
