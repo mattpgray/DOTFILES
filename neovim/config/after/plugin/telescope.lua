58869413055706df518db8669e9a29215ce8994f
mgray_altair <mgray@altair.com>
2023-06-16 10:18:10 +0100

    Search for all ignored files with pi

--- neovim/config/after/plugin/telescope.lua
+++ neovim/config/after/plugin/telescope.lua
@@ -1,7 +1,13 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pi', function()
-    builtin.find_files({ no_ignore = true })
+    builtin.find_files({
+        prompt_title = 'find all files including hidden and ignore and following symlinks',
+        hidden= true,
+        no_ignore = true,
+        follow = true,
+    })
+
 end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
