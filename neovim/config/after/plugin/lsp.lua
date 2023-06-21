c5edba095435085828377b17a3666c05a3248826
mgray_altair <mgray@altair.com>
2023-06-21 14:49:23 +0100

    nvim: make cspell info severity

--- neovim/config/after/plugin/lsp.lua
+++ neovim/config/after/plugin/lsp.lua
@@ -101,9 +101,15 @@ local cspell_config = {
     end,
 }
 
+
 null_ls.setup({
     sources = {
-        cspell.diagnostics.with({ config = cspell_config }),
+        cspell.diagnostics.with({
+            diagnostics_postprocess = function(diagnostic)
+                diagnostic.severity = vim.diagnostic.severity.INFO
+            end,
+            config = cspell_config,
+        }),
         cspell.code_actions.with({ config = cspell_config }),
     }
 })
