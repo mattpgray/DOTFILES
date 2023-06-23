8b6eb2ec8ffe588d5bac88f9b9665292b6c5e5e7
mgray_altair <mgray@altair.com>
2023-06-23 16:29:50 +0100

    nvim: Allow closing buffers from within telescope

--- neovim/config/after/plugin/telescope.lua
+++ neovim/config/after/plugin/telescope.lua
@@ -1,13 +1,14 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pi', function()
     builtin.find_files({
         prompt_title = 'find all files including hidden and ignore and following symlinks',
-        hidden= true,
+        hidden = true,
         no_ignore = true,
         follow = true,
     })
-
 end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -29,3 +30,20 @@ vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>pp', builtin.pickers, {})
+
+telescope.setup {
+    defaults = {
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        mappings = {
+            n = {
+                ['<c-d>'] = actions.delete_buffer
+            },
+            i = {
+                ["<C-h>"] = "which_key",
+                ['<c-d>'] = actions.delete_buffer
+            }
+        }
+    },
+}
