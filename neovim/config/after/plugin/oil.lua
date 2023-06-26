fe979c2abefd1596cab5a9c3ca50ead395ce0e87
mgray_altair <mgray@altair.com>
2023-06-26 09:58:24 +0100

    nvim: Open into oil when no file provided

---
+++ neovim/config/after/plugin/oil.lua
@@ -0,0 +1,9 @@
+local file_is_set = function()
+    return vim.api.nvim_buf_get_name(0) == ""
+end
+
+-- Stops a scratch buffer from being created when neovim is opened without a file provided, which
+-- has its pros and cons.
+if file_is_set() then
+    vim.cmd [[silent edit .]]
+end
