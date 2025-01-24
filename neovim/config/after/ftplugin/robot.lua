27d3c3a473a1cd45752a7daa442b9cb0e6385d56
Matthew Gray <matthewphilipgray@gmail.com>
2025-01-24 12:07:04 +0000

    nvim: Remove auto line splitting for robot files

---
+++ neovim/config/after/ftplugin/robot.lua
@@ -0,0 +1,4 @@
+-- It is easier to manage the wrapping of lines manually in robot due to the
+-- requirement of multiple spaces and starting lines with '...'.
+vim.opt.textwidth = 0
+vim.opt.wrap = false
